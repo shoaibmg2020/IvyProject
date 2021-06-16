53cc2df8f83aac27005b65be0a6aa5746d2127c5
shoaibmg2020 <smgcts2019@outlook.com>
2021-06-16 18:21:35 +0530

    Class1 is added

---
+++ MyFirstApp/MyFirstApp/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp
+{
+    class Class1
+    {
+        static void Main()
+        {
+            Console.WriteLine("This is Class1");
+            Console.ReadLine();
+        }
+    }
+}
